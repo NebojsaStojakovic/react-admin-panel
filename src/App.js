d947adc07d21af4e9f5a16c5c3a3c8a8a790d6c8
NebojsaStojakovic <nebojsastojakovicla@gmail.com>
2022-05-31 12:36:04 +0200

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Home from "./pages/home/Home";
+
 const App = () => {
-  return <div className='App'>Learn React</div>;
+  return (
+    <div className='App'>
+      <Home />
+    </div>
+  );
 };
 
 export default App;
